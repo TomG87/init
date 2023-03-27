3a9406429d5960b90b64dc94ef43bc6b564b5020
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-27 14:30:45 -0400

    added 2nd path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/hey_how_are_you", controller: "example_pages", action: "sports"
+  get "/hiya", controller: "example_pages", action: "glove"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
